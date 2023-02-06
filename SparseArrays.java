626f8eb5bfa2fc78d5f2dfccfbad94ba5317082e
Dmitriy <altusj@gmail.com>
2023-02-06 18:31:36 -0500

    new task

--- SparseArrays.java
+++ SparseArrays.java
@@ -1,3 +1,12 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.List;
+import java.util.stream.IntStream;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
 
 public class SparseArrays {
 
@@ -14,9 +23,45 @@ public class SparseArrays {
         
     }
     
-    public static void main(String[] args) {
-        // TODO Auto-generated method stub
+    public static void main(String[] args) throws IOException {
+            BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+            BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+            int stringsCount = Integer.parseInt(bufferedReader.readLine().trim());
+
+            List<String> strings = IntStream.range(0, stringsCount).mapToObj(i -> {
+                try {
+                    return bufferedReader.readLine();
+                } catch (IOException ex) {
+                    throw new RuntimeException(ex);
+                }
+            })
+                .collect(toList());
+
+            int queriesCount = Integer.parseInt(bufferedReader.readLine().trim());
+
+            List<String> queries = IntStream.range(0, queriesCount).mapToObj(i -> {
+                try {
+                    return bufferedReader.readLine();
+                } catch (IOException ex) {
+                    throw new RuntimeException(ex);
+                }
+            })
+                .collect(toList());
+
+            List<Integer> res = Result.matchingStrings(strings, queries);
+
+            bufferedWriter.write(
+                res.stream()
+                    .map(Object::toString)
+                    .collect(joining("\n"))
+                + "\n"
+            );
+
+            bufferedReader.close();
+            bufferedWriter.close();
+        }
 
     }
 
-}
+

d7425ba820256baec739ba99edd01c267c050243
Dmitriy <altusj@gmail.com>
2023-02-06 18:38:27 -0500

    add solution

--- SparseArrays.java
+++ SparseArrays.java
@@ -9,6 +9,21 @@ import java.util.stream.IntStream;
 import static java.util.stream.Collectors.joining;
 import static java.util.stream.Collectors.toList;
 
+/*
+ * There is a collection of input strings and a collection of query strings. 
+ * For each query string, determine how many times it occurs in the list of input strings. 
+ * Return an array of the results.
+
+Example:
+    strings = ["ab","ab","abc"]
+    queries = ["ab","abc","bc"]
+
+
+  * There are 2 instances of 'ab', 1 of 'abc' and 0 of 'bc'. 
+  * For each query, add an element to the return array, 
+  * results = [2,1,0] 
+ */
+
 class Result3 {
     
     /*

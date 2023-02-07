314f360459a29c4844c84366baea9c160897897f
Dmitriy <altusj@gmail.com>
2023-02-07 10:43:01 -0500

    add IOException

--- LonelyInteger.java
+++ LonelyInteger.java
@@ -1,6 +1,7 @@
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.FileWriter;
+import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.HashMap;
 import java.util.List;
@@ -57,7 +58,7 @@ class Result4 {
 
 public class LonelyInteger {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
 
@@ -76,6 +77,6 @@ public class LonelyInteger {
         bufferedWriter.close();
     }
 
-    }
+ 
 
 }

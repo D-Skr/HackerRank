01d505a1d5571f5e8b708d5b9cf771bf47914088
Dmitriy <altusj@gmail.com>
2023-02-07 10:45:11 -0500

    add new GCD recursion problem

---
+++ GCD.java
@@ -0,0 +1,10 @@
+//How to find GCD (Greatest Common Divisor) of two numbers using recursion?
+
+public class GCD {
+
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+
+    }
+
+}

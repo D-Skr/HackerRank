82398367ffedab1742f10c5a3fb81940c2e4be5a
Dmitriy <altusj@gmail.com>
2023-02-07 10:47:18 -0500

    add steps description

--- GCD.java
+++ GCD.java
@@ -1,7 +1,12 @@
 //How to find GCD (Greatest Common Divisor) of two numbers using recursion?
 
 public class GCD {
-
+    
+    /*
+     * Step 1: Recursive case - the flow
+     * Step 2: Base case - the stopping criterion
+     * Step 3: Unintentional case - the constraint
+     */
     public static void main(String[] args) {
         // TODO Auto-generated method stub
 

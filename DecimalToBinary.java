8a1cb345c8dda8eba21d1858b4c8054f21967ace
Dmitriy <altusj@gmail.com>
2023-02-07 12:00:07 -0500

    new problem Decimal to Binary

--- DecimalToBinary.java
+++ DecimalToBinary.java
@@ -1,5 +1,14 @@
+//converting number from Decimal to Binary using recorsion
 
 public class DecimalToBinary {
+    
+    public static int decToBin(int n) {
+        
+        if(a == 0) {
+
+        }
+        return n%2 + decToBin(n/2);
+    }
 
     public static void main(String[] args) {
         // TODO Auto-generated method stub

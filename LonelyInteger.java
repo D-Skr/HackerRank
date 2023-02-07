80f06b45de11c0dad089945435f74defac5ac834
Dmitriy <altusj@gmail.com>
2023-02-07 10:43:30 -0500

    add IOException

--- LonelyInteger.java
+++ LonelyInteger.java
@@ -7,7 +7,6 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Stream;
-import static java.util.stream.Collectors.joining;
 import static java.util.stream.Collectors.toList;
 
 /*

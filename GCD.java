decce09a672a30669a429002f6f4f69288384912
Dmitriy <altusj@gmail.com>
2023-02-07 10:50:24 -0500

    add step 1

--- GCD.java
+++ GCD.java
@@ -7,8 +7,17 @@ public class GCD {
      * Step 2: Base case - the stopping criterion
      * Step 3: Unintentional case - the constraint
      */
+
+    //step 1: Euclidean algorithm
+    public static int gcd(int a, int b) {
+    
+        return gcd(b, a % b);
+    }
+    
     public static void main(String[] args) {
-        // TODO Auto-generated method stub
+        
+        
+        
 
     }
 

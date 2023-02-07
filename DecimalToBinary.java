9ee24660b62b8a2947f8dbe1674d63c2374faf26
Dmitriy <altusj@gmail.com>
2023-02-07 12:05:22 -0500

    add description

--- DecimalToBinary.java
+++ DecimalToBinary.java
@@ -2,6 +2,13 @@
 
 public class DecimalToBinary {
     
+    /*
+     *Step 1: Divide the number by 2
+     *Step 2: Get the integer quotient for the next iteration
+     *Step 3: Get the remainder for the binary digit
+     *Step 4: Repeat the steps until the quotient is equal to 0  
+     */
+    
     public static int decToBin(int n) {
         
         if(n == 0) {

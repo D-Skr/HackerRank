f78ccb1c648658861586f8876e2db5ad0605d0fe
Dmitriy <altusj@gmail.com>
2023-02-12 14:58:12 -0500

    add solution for Flipping Bits

--- FlippingBits.java
+++ FlippingBits.java
@@ -1,3 +1,9 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.stream.IntStream;
 
 public class FlippingBits {
     
@@ -28,7 +34,26 @@ public class FlippingBits {
     
     
     public static void main(String[] args) {
-        // TODO Auto-generated method stub
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int q = Integer.parseInt(bufferedReader.readLine().trim());
+
+        IntStream.range(0, q).forEach(qItr -> {
+            try {
+                long n = Long.parseLong(bufferedReader.readLine().trim());
+
+                long result = flippingBits(n);
+
+                bufferedWriter.write(String.valueOf(result));
+                bufferedWriter.newLine();
+            } catch (IOException ex) {
+                throw new RuntimeException(ex);
+            }
+        });
+
+        bufferedReader.close();
+        bufferedWriter.close();
 
     }
 
